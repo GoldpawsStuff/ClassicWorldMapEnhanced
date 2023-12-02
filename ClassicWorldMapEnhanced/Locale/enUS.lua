235f7f8bc17bd98c784d9552507abe4444f87170
Lars Norberg <github@larsnorbergofficial.com>
2023-12-02 12:21:12 +0100

    update locales

--- ClassicWorldMapEnhanced/Locale/enUS.lua
+++ ClassicWorldMapEnhanced/Locale/enUS.lua
@@ -1,4 +1,15 @@
 local L = LibStub("AceLocale-3.0"):NewLocale((...), "enUS", true, true)
 
-L["Fog of War"] = true
+L["Enable the fog of war, covering the areas of the map you haven't yet explored."] = true
+L["Enhanced WorldMap"] = true
 L["Fade when moving"] = true
+L["Fades the map out when moving to allow you to see your character and its closest surroundings."] = true
+L["Fog of War"] = true
+L["Map opacity"] = true
+L["Map opacity when moving"] = true
+L["Sets the map opacity when moving."] = true
+L["Sets the map opacity when not moving."] = true
+L["Show cursor coordinates"] = true
+L["Show map coordinates of the mouse cursor."] = true
+L["Show map coordinates of the player's current location."] = true
+L["Show player coordinates"] = true
